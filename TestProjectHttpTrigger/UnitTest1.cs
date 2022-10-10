678086bd15a8fb077a9e6aabe133fe5b8f323481
Frank Dersjant <36342252+frankdersjant@users.noreply.github.com>
2022-10-10 17:10:35 +0200

    first

---
+++ TestProjectHttpTrigger/UnitTest1.cs
@@ -0,0 +1,36 @@
+using FunctionApp6;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Moq;
+using System.Web.Http;
+using Xunit;
+
+namespace TestProjectHttpTrigger
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData("", typeof(BadRequestResult))]
+        [InlineData("QueryParamValue", typeof(OkResult))]
+        [InlineData("ThisStringCausesTheFunctionToThrowAnError", typeof(InternalServerErrorResult))]
+        public async Task Function_Returns_Correct_StatusCode(string queryParam, Type expectedResult)
+        {
+            //Arrange
+            var qc = new QueryCollection(new Dictionary<string, StringValues> { { "q", new StringValues(queryParam) } });
+            var request = new Mock<HttpRequest>();
+            request.Setup(x => x.Query)
+                .Returns(() => qc);
+
+            var logger = Mock.Of<ILogger>();
+            
+            //Act
+            var response = await FunctionHttpTrigger.Run(request.Object, logger);
+            
+            //Assert
+            Xunit.Assert.True(response.GetType() == expectedResult);
+        }
+    }
+}
